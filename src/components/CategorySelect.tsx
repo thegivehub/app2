cd2c66bbcdbcf162476784a0707a77e15d16f5f5
Kevin Ready <kevin.ready@gmail.com>
2024-12-11 11:47:25 -0800

    Adding files, removing yarn and npm lock files

--- src/components/CategorySelect.tsx
+++ src/components/CategorySelect.tsx
@@ -1,86 +1,88 @@
-import {forwardRef} from 'react';
-import {Group, Select, Text} from "@mantine/core";
+import { forwardRef } from 'react';
+import { Group, Select, Text } from '@mantine/core';
 import {
-    IconAugmentedReality,
-    IconCat,
-    IconClipboardHeart,
-    IconDeviceTv,
-    IconFireHydrant,
-    IconHeartHandshake,
-    IconLeaf,
-    IconReportMoney,
-    IconSos
-} from "@tabler/icons-react";
+  IconAugmentedReality,
+  IconCat,
+  IconClipboardHeart,
+  IconDeviceTv,
+  IconFireHydrant,
+  IconHeartHandshake,
+  IconLeaf,
+  IconReportMoney,
+  IconSos,
+} from '@tabler/icons-react';
 
 const mockdata = [
-    {
-        icon: IconClipboardHeart,
-        title: 'Medical',
-    },
-    {
-        icon: IconSos,
-        title: 'Emergency',
-    },
-    {
-        icon: IconLeaf,
-        title: 'Environment',
-    },
-    {
-        icon: IconHeartHandshake,
-        title: 'Nonprofit',
-    },
-    {
-        icon: IconReportMoney,
-        title: 'Financial emergency',
-    },
-    {
-        icon: IconCat,
-        title: 'Animals',
-    },
-    {
-        icon: IconFireHydrant,
-        title: 'Crisis Relief',
-    },
-    {
-        icon: IconAugmentedReality,
-        title: 'Technology',
-    },
-    {
-        icon: IconDeviceTv,
-        title: 'Film & Videos',
-    },
+  {
+    icon: IconClipboardHeart,
+    title: 'Medical',
+  },
+  {
+    icon: IconSos,
+    title: 'Emergency',
+  },
+  {
+    icon: IconLeaf,
+    title: 'Environment',
+  },
+  {
+    icon: IconHeartHandshake,
+    title: 'Nonprofit',
+  },
+  {
+    icon: IconReportMoney,
+    title: 'Financial emergency',
+  },
+  {
+    icon: IconCat,
+    title: 'Animals',
+  },
+  {
+    icon: IconFireHydrant,
+    title: 'Crisis Relief',
+  },
+  {
+    icon: IconAugmentedReality,
+    title: 'Technology',
+  },
+  {
+    icon: IconDeviceTv,
+    title: 'Film & Videos',
+  },
 ];
 
 const CategorySelectItem = forwardRef<HTMLDivElement, any>(
-    ({title, ...others}: any, ref) => (
-        <div ref={ref} {...others}>
-            <Group noWrap>
-                <others.icon size={18}/>
+  ({ title, ...others }: any, ref) => (
+    <div ref={ref} {...others}>
+      <Group noWrap>
+        <others.icon size={18} />
 
-                <div>
-                    <Text size="sm">{title}</Text>
-                </div>
-            </Group>
+        <div>
+          <Text size="sm">{title}</Text>
         </div>
-    )
+      </Group>
+    </div>
+  )
 );
 
-
 const CategorySelect = () => {
-    return (
-        <Select
-            label="Category"
-            itemComponent={CategorySelectItem}
-            data={mockdata.map(c => ({value: c.title, label: c.title, ...c}))}
-            searchable
-            clearable
-            maxDropdownHeight={300}
-            nothingFound="Nothing found"
-            filter={(value, item) =>
-                item?.title?.toLowerCase().includes(value?.toLowerCase().trim())
-            }
-        />
-    );
+  return (
+    <Select
+      label="Category"
+      itemComponent={CategorySelectItem}
+      data={mockdata.map((c) => ({ value: c.title, label: c.title, ...c }))}
+      searchable
+      clearable
+      maxDropdownHeight={300}
+      nothingFound="Nothing found"
+      filter={(value, item) =>
+        item?.title?.toLowerCase().includes(value?.toLowerCase().trim())
+      }
+      placeholder="Select category"
+      required
+      aria-required
+    />
+  );
 };
 
 export default CategorySelect;
